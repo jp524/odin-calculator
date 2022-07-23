c14f7c97af2f7e6d706da7cd697eeb83feda912f
JP <85654561+jp524@users.noreply.github.com>
2022-07-23 18:35:35 -0400

    Build compute() to work with two integers and one operator

--- calculator.js
+++ calculator.js
@@ -1,28 +1,28 @@
 function add(a, b) {
-  return a + b
+  return +a + +b
 }
 
 function subtract(a, b) {
-  return a - b;
+  return +a - +b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return +a * +b;
 }
 
 function divide(a, b) {
-  return a / b;
+  return +a / +b;
 }
 
 function operate(operator, a, b) {
   switch (operator) {
-    case "add":
+    case "+":
       return add(a, b);
-    case "substract":
+    case "-":
       return subtract(a, b);
-    case "multiply":
+    case "*":
       return multiply(a, b);
-    case "divide":
+    case "/":
       return divide(a, b);
     default:
       console.log("ERROR");
@@ -31,22 +31,47 @@ function operate(operator, a, b) {
 
 let input = "";
 display = document.querySelector(".display");
-
 function writeDisplay(value) {
   input += value;
   display.textContent = input;
 }
 
+function clear() {
+  input = "";
+  display.textContent = input;
+}
+
+function splitInput(input) {
+  const OPERATOR_OPTIONS = ["+", "-", "*", "/"];
+  const l = input.length;
+  let a = "";
+  let b = "";
+  let operator = "";
+  for (let i = 0; i < l; i++) {
+    if (OPERATOR_OPTIONS.includes(input[i])) {
+      a = input.slice(0, i);
+      operator = input.slice(i, i + 1);
+      b = input.slice(i + 1, l);
+      return toCompute = { a, b, operator };
+    };
+  };
+}
+
+function compute() {
+  let {a, b, operator} = splitInput(input);
+  display.textContent = operate(operator, a, b);
+}
+
 buttons = document.querySelectorAll("button");
 buttons.forEach(button => button.addEventListener("click", () => {
   if (button.id === "clear") {
-    console.log("Call clear function");
+    clear();
   } else if (button.id === "equal") {
-    console.log("Call compute function");
+    compute();
   } else {
     writeDisplay(button.value);
-  }
-}))
+  };
+}));
 
 // Testing
 module.exports = {
